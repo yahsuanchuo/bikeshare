10a50b9b3cc5441d057276785e58b22b50a5cf7a
yahsuanchuo <amychuo41131@gmail.com>
2017-02-01 18:41:45 -0500

    Practice

---
+++ jerry.R
@@ -0,0 +1,22 @@
+#2
+length(data$Gender[data$Gender=="M"])
+summary(data$Gender) #57096
+
+#3
+dim(subset(data,data$Gender=="F"& data$any_aid=="TRUE")) #367
+
+#4
+class <- data$WealthRanking
+sapply(data, class)
+mean(data, )
+
+tapply(data$ActualWeight,data$Age,quantile)
+
+#5
+tapply(data$ActualWeight,data$Age,function(t) quantile(t, prob=seq(0,1,0.05)))
+tapply(data$ActualWeight,data$Age,function(t) quantile(t, prob=c(0.05,0.5,0.95)))
+
+#6
+data$NewID <- as.factor(data$ChildHHID) 
+
+data$diff <- unlist(tapply(data$ActualWeight,data$Age, function(x) c(NA,diff(x))))
